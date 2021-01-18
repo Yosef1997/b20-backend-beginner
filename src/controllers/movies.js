878382585c58b6084867bc06fafb37308dd54bda
Irul <fulsep@gmail.com>
2021-01-18 13:11:00 +0700

    integrate db

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -1,52 +1,118 @@
-const { LIMIT_DATA, APP_URL } = process.env
+// const { LIMIT_DATA, APP_URL } = process.env
+const movieModel = require('../models/movies')
 
 exports.listMovies = (req, res) => {
-  const { limit = LIMIT_DATA, search = null } = req.query
-  let { page = 1 } = req.query
-  if (typeof (page) !== 'number') {
-    page = Number(page)
-  }
-  const data = require('../helpers/listMovies')
-  const paging = (page * limit) - limit
-  const offset = limit * page
-  const nextPage = ((page + 1) * limit) - limit
-  const nextPageOffset = limit * (page + 1)
-  let nextPageData = []
+  const cond = req.query
+  cond.search = cond.search || ''
+  cond.page = Number(cond.page) || 1
+  cond.limit = Number(cond.limit) || 5
+  cond.dataLimit = cond.limit * cond.page
+  cond.offset = (cond.page - 1) * cond.limit
+  cond.sort = cond.sort || 'id'
+  cond.order = cond.order || 'ASC'
 
-  let results = []
-
-  if (search) {
-    results = data.filter(movie => {
-      console.log(movie.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-      return movie.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  movieModel.getMoviesByCondition(cond, results => {
+    return res.json({
+      success: true,
+      message: 'List of all Movies',
+      results
     })
-    nextPageData = results.slice(nextPage, nextPageOffset)
-    results = results.slice(paging, offset)
-  } else {
-    nextPageData = data.slice(nextPage, nextPageOffset)
-    // console.log(nextPageData)
-    results = data.slice(paging, offset)
-  }
-
-  return res.json({
-    success: true,
-    message: 'List of Movies',
-    results,
-    pageInfo: {
-      totalData: results.length,
-      currentPage: page,
-      nextLink: nextPageData.length > 0 ? `${APP_URL}/movies?page=${page + 1}` : null,
-      prevLink: page > 1 ? `${APP_URL}/movies?page=${page - 1}` : null
-    }
   })
 }
 
 exports.detailMovies = (req, res) => {
-  return res.json({
-    success: true,
-    message: 'Details of Movie',
-    results: {
+  const { id } = req.params
+  movieModel.getMovieById(id, results => {
+    if (results.length > 0) {
+      return res.json({
+        success: true,
+        message: 'Details of Movie',
+        results: results[0]
+      })
+    }
+    return res.status(400).json({
+      success: false,
+      message: 'Movies not exists'
+    })
+  })
+}
 
+exports.createMovies = (req, res) => {
+  const data = req.body
+  movieModel.createMovies(data, (results) => {
+    if (results.affectedRows > 0) {
+      movieModel.getMovieById(results.insertId, (finalResult) => {
+        if (finalResult.length > 0) {
+          return res.json({
+            success: true,
+            message: 'Details of Movie',
+            results: finalResult[0]
+          })
+        }
+        return res.status(400).json({
+          success: false,
+          message: 'Failed to create Movie'
+        })
+      })
+    }
+  })
+}
+
+exports.deleteMovie = async (req, res) => {
+  const { id } = req.params
+  const initialResult = await movieModel.getMovieByIdAsync(id)
+  if (initialResult.length > 0) {
+    const results = await movieModel.getMovieByIdAsync(id)
+    if (results) {
+      return res.json({
+        success: true,
+        message: 'Data deleted successfully',
+        results: initialResult[0]
+      })
+    }
+  }
+  return res.json({
+    success: false,
+    message: 'Failed to delete data'
+  })
+  // movieModel.getMovieById(id, (initialResult) => {
+  //   if (initialResult.length > 0) {
+  //     movieModel.deleteMovieById(id, results => {
+  //       return res.json({
+  //         success: true,
+  //         message: 'Data deleted successfully',
+  //         results: initialResult[0]
+  //       })
+  //     })
+  //   } else {
+  //     return res.json({
+  //       success: false,
+  //       message: 'Failed to delete data'
+  //     })
+  //   }
+  // })
+}
+
+exports.updateMovie = (req, res) => {
+  const { id } = req.params
+  const data = req.body
+  movieModel.getMovieById(id, initialResult => {
+    if (initialResult.length > 0) {
+      movieModel.updateMovie(id, data, results => {
+        return res.json({
+          success: true,
+          message: 'Update data success',
+          results: {
+            ...initialResult[0],
+            ...data
+          }
+        })
+      })
+    } else {
+      return res.json({
+        success: false,
+        message: 'Failed to update data'
+      })
     }
   })
 }
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -3,5 +3,8 @@ const moviesController = require('../controllers/movies')
 
 routes.get('/', moviesController.listMovies)
 routes.get('/:id', moviesController.detailMovies)
+routes.post('/', moviesController.createMovies)
+routes.delete('/:id', moviesController.deleteMovie)
+routes.patch('/:id', moviesController.updateMovie)
 
 module.exports = routes
